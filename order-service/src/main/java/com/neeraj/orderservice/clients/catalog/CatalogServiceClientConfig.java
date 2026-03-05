091cae2ba64528a15ddaaf29189c397e96c2d33e
NR3101 <neeraj2000rai@gmail.com>
2026-03-05 21:02:44 +0530

    feat: add retry to our rest client and add resiliency to our services using resilience4j retry and circuit breaker in catalog service client

--- order-service/src/main/java/com/neeraj/orderservice/clients/catalog/CatalogServiceClientConfig.java
+++ order-service/src/main/java/com/neeraj/orderservice/clients/catalog/CatalogServiceClientConfig.java
@@ -1,14 +1,27 @@
 package com.neeraj.orderservice.clients.catalog;
 
 import com.neeraj.orderservice.ApplicationProperties;
+import java.time.Duration;
+import org.springframework.boot.http.client.ClientHttpRequestFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
-public class CatalogServiceClientConfig {
+class CatalogServiceClientConfig {
     @Bean
-    RestClient restClient(ApplicationProperties properties) {
-        return RestClient.builder().baseUrl(properties.catalogServiceUrl()).build();
+    RestClient restClient(RestClient.Builder builder, ApplicationProperties properties) {
+        // Configure timeouts for the RestClient using ClientHttpRequestFactoryBuilder
+        ClientHttpRequestFactory requestFactory = ClientHttpRequestFactoryBuilder.simple()
+                .withCustomizer(customizer -> {
+                    customizer.setConnectTimeout(Duration.ofSeconds(5));
+                    customizer.setReadTimeout(Duration.ofSeconds(5));
+                })
+                .build();
+
+        return builder.baseUrl(properties.catalogServiceUrl())
+                .requestFactory(requestFactory)
+                .build();
     }
 }
--- order-service/src/main/java/com/neeraj/orderservice/clients/catalog/ProductServiceClient.java
+++ order-service/src/main/java/com/neeraj/orderservice/clients/catalog/ProductServiceClient.java
@@ -1,5 +1,7 @@
 package com.neeraj.orderservice.clients.catalog;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -13,19 +15,27 @@ public class ProductServiceClient {
 
     private final RestClient restClient;
 
+    // Method to fetch product details by code with retry, circuit breaker, and fallback
+    @CircuitBreaker(name = "catalog-service")
+    @Retry(name = "catalog-service", fallbackMethod = "getProductByCodeFallback")
     public Optional<Product> getProductByCode(String code) {
         log.info("Fetching product details for code: {}", code);
-        try {
-            var product = restClient
-                    .get()
-                    .uri("/api/products/{code}", code)
-                    .retrieve()
-                    .body(Product.class);
 
-            return Optional.ofNullable(product);
-        } catch (Exception e) {
-            log.error("Error fetching product details for code: {}", code, e);
-            return Optional.empty();
-        }
+        var product =
+                restClient.get().uri("/api/products/{code}", code).retrieve().body(Product.class);
+
+        return Optional.ofNullable(product);
+    }
+
+    /*
+     * NOTE: 1. The signature of the fallback method must match the original method's parameters plus an additional Throwable parameter to capture the exception that caused the fallback. The return type should also match the original method's return type.
+     * 2. In case we use both @Retry and @CircuitBreaker annotations,it is best to use fallback for only @Retry, as the fallback will be triggered for each retry attempt, allowing you to handle transient failures more effectively. If you use a fallback for @CircuitBreaker, it will only be triggered when the circuit breaker is open, which may not provide the same level of resilience for transient issues.
+     * 3. The order of resilience annotations is usually @Retry followed by @CircuitBreaker.
+     */
+
+    // Fallback method to return an empty Optional when the product details cannot be fetched.
+    public Optional<Product> getProductByCodeFallback(String code, Throwable ex) {
+        log.error("Failed to fetch product details for code: {}. Error: {}", code, ex.getMessage());
+        return Optional.empty();
     }
 }
