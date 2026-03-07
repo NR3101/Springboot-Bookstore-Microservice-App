0b9384e993c5d2c055bd63e89d409cc38fbaee1f
NR3101 <neeraj2000rai@gmail.com>
2026-03-07 12:50:56 +0530

    feat: add ShedLock for scheduled job locking so that only node process the same job at a time and create shedlock table

--- order-service/src/main/java/com/neeraj/orderservice/jobs/OrderEventsPublishingJob.java
+++ order-service/src/main/java/com/neeraj/orderservice/jobs/OrderEventsPublishingJob.java
@@ -4,6 +4,8 @@ import com.neeraj.orderservice.domain.OrderEventService;
 import java.time.Instant;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import net.javacrumbs.shedlock.core.LockAssert;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -15,7 +17,10 @@ public class OrderEventsPublishingJob {
     private final OrderEventService orderEventService;
 
     @Scheduled(cron = "${orders.publish-order-events-job-cron}")
+    @SchedulerLock(name = "publishOrderEvents")
     public void publishOrderEvents() {
+        LockAssert.assertLocked();
+
         log.info("Starting Order Events Publishing Job at: {}", Instant.now());
         orderEventService.publishOrderEvents();
     }
--- order-service/src/main/java/com/neeraj/orderservice/jobs/OrderProcessingJob.java
+++ order-service/src/main/java/com/neeraj/orderservice/jobs/OrderProcessingJob.java
@@ -4,6 +4,8 @@ import com.neeraj.orderservice.domain.OrderService;
 import java.time.Instant;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import net.javacrumbs.shedlock.core.LockAssert;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -15,7 +17,10 @@ public class OrderProcessingJob {
     private final OrderService orderService;
 
     @Scheduled(cron = "${orders.process-new-orders-job-cron}")
+    @SchedulerLock(name = "processNewOrders")
     public void processNewOrders() {
+        LockAssert.assertLocked();
+
         log.info("Starting New Orders Processing Job at: {}", Instant.now());
         orderService.processNewOrders();
     }
